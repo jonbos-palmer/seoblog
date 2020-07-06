3bdcb56f0550de1f7bab0fa102224d36311d9949
jonbos-palmer <jbos@thepalmergroup.com>
2020-07-06 09:17:32 -0500

    add signin route

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,8 +3,13 @@ const { signup } = require("../controllers/auth");
 
 //validators
 const { runValidation } = require("../validators/");
-const { userSignupValidator } = require("../validators/auth");
+const {
+    userSignupValidator,
+    userSignInValidator,
+} = require("../validators/auth");
 
 router.post("/signup", userSignupValidator, runValidation, signup);
+
+router.post("/signin", userSignInValidator, runValidation, signup);
 
 module.exports = router;
