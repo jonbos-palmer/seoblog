19eb3d933359fd6835785eb4159d86cc9bf5e496
jonbos-palmer <jbos@thepalmergroup.com>
2020-07-09 10:14:02 -0500

    add methods for get/set cookie

--- frontend/actions/auth.js
+++ frontend/actions/auth.js
@@ -1,6 +1,6 @@
 import fetch from "isomorphic-fetch";
 import { API } from "../config";
-
+import { cookie } from "js-cookie";
 export const signup = (user) => {
   return fetch(`${API}/signup`, {
     method: "POST",
@@ -29,4 +29,58 @@ export const signin = (user) => {
       return response.json();
     })
     .catch((err) => console.log(err));
+};
+
+// set cookie
+export const setCookie = (key, value) => {
+  if (process.browser) {
+    cookie.set(key, value, {
+      expires: 1,
+    });
+  }
+};
+
+export const removeCookie = (key) => {
+  if (process.browser) {
+    cookie.remove(key);
+  }
+};
+
+// get cookie
+export const getCookie = (key) => {
+  if (process.browser) {
+    cookie.get(key);
+  }
+};
+// localstorage
+export const setLocalStorage = (key, value) => {
+  if (process.browser) {
+    localStorage.setItem(key, JSON.stringify(value));
+  }
+};
+
+export const removeLocalStorage = (key) => {
+  if (process.browser) {
+    localStorage.removeItem(key);
+  }
+};
+
+// authenticate
+export const authenticate = (data, next) => {
+  setCookie("token", data.token);
+  setLocalStorage("user", data.user);
+  next();
+};
+
+export const isAuth = () => {
+  if (process.browser) {
+    const cookieChecked = getCookie("token");
+    if (cookieChecked) {
+      if (localStorage.getItem("user")) {
+        return JSON.parse(localStorage.getItem("user"));
+      } else {
+        return false;
+      }
+    }
+  }
 };
