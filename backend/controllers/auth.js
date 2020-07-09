96b5a5332143d979394f8bbef3b05d70f5e713a2
jonbos-palmer <jbos@thepalmergroup.com>
2020-07-09 14:19:32 -0500

    auth and admin middleware

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -3,65 +3,106 @@ const shortId = require("shortid");
 const jwt = require("jsonwebtoken");
 const expressJwt = require("express-jwt");
 
+exports.read = (req, res) => {
+  req.profile.hashed_password = undefinedl;
+  return res.json(req.profile);
+};
 exports.signup = (req, res) => {
-    User.findOne({ email: req.body.email }).exec((err, user) => {
-        if (user) {
-            return res.status(400).json({ error: "username is taken" });
-        }
-        const { name, email, password } = req.body;
-        let username = shortId.generate();
-        let profile = `${process.env.CLIENT_URL}/profile/${username}`;
+  User.findOne({ email: req.body.email }).exec((err, user) => {
+    if (user) {
+      return res.status(400).json({ error: "username is taken" });
+    }
+    const { name, email, password } = req.body;
+    let username = shortId.generate();
+    let profile = `${process.env.CLIENT_URL}/profile/${username}`;
 
-        let newUser = new User({ name, email, password, profile, username });
-        newUser.save((err, success) => {
-            if (err) {
-                return res.status(400).json({
-                    error: err,
-                });
-            }
-            res.json({ message: "signup successful" });
+    let newUser = new User({ name, email, password, profile, username });
+    newUser.save((err, success) => {
+      if (err) {
+        return res.status(400).json({
+          error: err,
         });
+      }
+      res.json({ message: "signup successful" });
     });
+  });
 };
 
 exports.signin = (req, res) => {
-    const { email, password } = req.body;
-    // check if user exists
-    User.findOne({ email }).exec((err, user) => {
-        if (err || !user) {
-            return res.status(400).json({
-                error: "User with that email does not exist. Please signup.",
-            });
-        }
-        // authenticate
-        if (!user.authenticate(password)) {
-            return res.status(400).json({
-                error: "Invalid Credentials",
-            });
-        }
+  const { email, password } = req.body;
+  // check if user exists
+  User.findOne({ email }).exec((err, user) => {
+    if (err || !user) {
+      return res.status(400).json({
+        error: "User with that email does not exist. Please signup.",
+      });
+    }
+    // authenticate
+    if (!user.authenticate(password)) {
+      return res.status(400).json({
+        error: "Invalid Credentials",
+      });
+    }
 
-        // generate a token and send to client
-        const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
-            expiresIn: "1 d",
-        });
-
-        res.cookie("token", token, { expiresIn: "1 d" });
-
-        const { _id, username, name, email, role } = user;
-
-        return res.json({
-            token,
-            user,
-        });
+    // generate a token and send to client
+    const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1 d",
     });
+
+    res.cookie("token", token, { expiresIn: "1 d" });
+
+    const { _id, username, name, email, role } = user;
+
+    return res.json({
+      token,
+      user,
+    });
+  });
 };
 
 exports.signout = (req, res) => {
-    res.clearCookie("token");
-    res.json({ message: "Signout success" });
+  res.clearCookie("token");
+  res.json({ message: "Signout success" });
 };
 
 exports.requireSignin = expressJwt({
-    secret: process.env.JWT_SECRET,
-    algorithms: ["HS256"],
+  secret: process.env.JWT_SECRET,
+  algorithms: ["HS256"],
 });
+
+exports.authMiddleware = (req, res, next) => {
+  const authUserId = req.user._id;
+  User.findById(_id).exec((err, user) => {
+    if (err || !user) {
+      return res.status(400).json({ error: "user not found" });
+    }
+    req.profile = user;
+    next();
+  });
+};
+
+exports.authMiddleware = (req, res, next) => {
+  const authUserId = req.user._id;
+  User.findById({ _id: authUserId }).exec((err, user) => {
+    if (err || !user) {
+      return res.status(400).json({ error: "user not found" });
+    }
+    req.profile = user;
+    next();
+  });
+};
+
+exports.adminMiddleware = (req, res, next) => {
+  const adminUserId = req.user._id;
+  User.findById({ _id: adminUserId }).exec((err, user) => {
+    if (err || !user) {
+      return res.status(400).json({ error: "user not found" });
+    }
+
+    if (user.role !== 1) {
+      return res.status(400).json({ error: "Admin resource. Access denied." });
+    }
+    req.profile = user;
+    next();
+  });
+};
