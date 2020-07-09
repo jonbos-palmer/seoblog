4a95683545400cd7cc981337ddbeaa0072ab8948
jonbos-palmer <jbos@thepalmergroup.com>
2020-07-09 13:49:51 -0500

    remove unused test route

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,16 +1,16 @@
 const router = require("express").Router();
 const {
-    signup,
-    signin,
-    signout,
-    requireSignin,
+  signup,
+  signin,
+  signout,
+  requireSignin,
 } = require("../controllers/auth");
 
 //validators
 const { runValidation } = require("../validators/");
 const {
-    userSignupValidator,
-    userSignInValidator,
+  userSignupValidator,
+  userSignInValidator,
 } = require("../validators/auth");
 
 router.post("/signup", userSignupValidator, runValidation, signup);
@@ -18,11 +18,5 @@ router.post("/signup", userSignupValidator, runValidation, signup);
 router.post("/signin", userSignInValidator, runValidation, signin);
 
 router.get("/signout", requireSignin, signout);
-
-router.get("/secret", requireSignin, (req, res) => {
-    res.json({
-        message: "you have access",
-    });
-});
 
 module.exports = router;
