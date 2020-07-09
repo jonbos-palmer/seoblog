ae2de3a7b2f478c456e832f726b02b7b29d7b193
jonbos-palmer <jbos@thepalmergroup.com>
2020-07-09 14:50:04 -0500

    fix user schema

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,89 +1,92 @@
 const mongoose = require("mongoose");
 const crypto = require("crypto");
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     username: {
-        type: String,
-        trim: true,
-        required: true,
-        max: 32,
-        unique: true,
-        index: true,
-        lowercase: true,
+      type: String,
+      trim: true,
+      required: true,
+      max: 32,
+      unique: true,
+      index: true,
+      lowercase: true,
     },
     name: {
-        type: String,
-        trim: true,
-        required: true,
-        maxlength: 32,
+      type: String,
+      trim: true,
+      required: true,
+      maxlength: 32,
     },
     email: {
-        type: String,
-        trim: true,
-        required: true,
-        max: 32,
-        unique: true,
-        lowercase: true,
+      type: String,
+      trim: true,
+      required: true,
+      max: 32,
+      unique: true,
+      lowercase: true,
     },
     profile: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     hashedPassword: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
 
     salt: String,
     about: {
-        type: String,
+      type: String,
     },
     role: {
-        type: Number,
-        trim: true,
+      type: Number,
+      default: 0,
     },
     photo: {
-        data: Buffer,
-        contentType: String,
+      data: Buffer,
+      contentType: String,
     },
     resetPasswordLink: {
-        data: String,
-        default: "",
+      data: String,
+      default: "",
     },
-}, { timestamps: true });
+  },
+  { timestamps: true }
+);
 
 userSchema
-    .virtual("password")
-    .set(function(password) {
-        //create temp var called _password
-        this._password = password;
-        // generate salt
-        this.salt = this.makeSalt();
-        //encrypt pw + salt
-        this.hashedPassword = this.encryptPassword(password);
-    })
-    .get(function() {
-        return this._password;
-    });
+  .virtual("password")
+  .set(function (password) {
+    //create temp var called _password
+    this._password = password;
+    // generate salt
+    this.salt = this.makeSalt();
+    //encrypt pw + salt
+    this.hashedPassword = this.encryptPassword(password);
+  })
+  .get(function () {
+    return this._password;
+  });
 
 userSchema.methods = {
-    authenticate: function(plainText) {
-        return this.encryptPassword(plainText) === this.hashedPassword;
-    },
-    encryptPassword: function(password) {
-        if (!password) return "";
-        try {
-            return crypto
-                .createHmac("sha1", this.salt)
-                .update(password)
-                .digest("hex");
-        } catch (err) {
-            return "";
-        }
-    },
-    makeSalt: function() {
-        return Math.round(new Date().valueOf() * Math.random()) + "";
-    },
+  authenticate: function (plainText) {
+    return this.encryptPassword(plainText) === this.hashedPassword;
+  },
+  encryptPassword: function (password) {
+    if (!password) return "";
+    try {
+      return crypto
+        .createHmac("sha1", this.salt)
+        .update(password)
+        .digest("hex");
+    } catch (err) {
+      return "";
+    }
+  },
+  makeSalt: function () {
+    return Math.round(new Date().valueOf() * Math.random()) + "";
+  },
 };
 
 module.exports = mongoose.model("User", userSchema);
