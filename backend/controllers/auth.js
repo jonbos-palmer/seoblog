5a11391067b4fdff69e341a5a254ef8f195d027a
jonbos-palmer <jbos@thepalmergroup.com>
2020-07-10 09:26:08 -0500

    remove duplicate middleware

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -68,17 +68,6 @@ exports.requireSignin = expressJwt({
 
 exports.authMiddleware = (req, res, next) => {
   const authUserId = req.user._id;
-  User.findById(_id).exec((err, user) => {
-    if (err || !user) {
-      return res.status(400).json({ error: "user not found" });
-    }
-    req.profile = user;
-    next();
-  });
-};
-
-exports.authMiddleware = (req, res, next) => {
-  const authUserId = req.user._id;
   User.findById({ _id: authUserId }).exec((err, user) => {
     if (err || !user) {
       return res.status(400).json({ error: "user not found" });
