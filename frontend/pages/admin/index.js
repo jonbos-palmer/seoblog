f5f48e6eaeb381539d854500a4754794cadd083b
jonbos-palmer <jbos@thepalmergroup.com>
2020-07-10 08:18:55 -0500

    add admin and user dashboards

---
+++ frontend/pages/admin/index.js
@@ -0,0 +1,8 @@
+import Layout from "../../components/Layout";
+import Link from "next/link";
+const AdminIndex = () => (
+  <Layout>
+    <h2>Home Page</h2>
+  </Layout>
+);
+export default Index;
---
+++ frontend/pages/user/index.js
@@ -0,0 +1,8 @@
+import Layout from "../../components/Layout";
+import Link from "next/link";
+const UserIndex = () => (
+  <Layout>
+    <h2>Home Page</h2>
+  </Layout>
+);
+export default Index;
