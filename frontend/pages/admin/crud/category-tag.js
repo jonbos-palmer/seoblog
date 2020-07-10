82e72eae1792ea3b449e15c9c6d6595b98f7777e
jonbos-palmer <jbos@thepalmergroup.com>
2020-07-10 16:19:41 -0500

    add tag management component

--- frontend/pages/admin/crud/category-tag.js
+++ frontend/pages/admin/crud/category-tag.js
@@ -2,25 +2,27 @@ import Layout from "../../../components/Layout";
 import Link from "next/link";
 import Admin from "../../../components/auth/Admin";
 import Category from "../../../components/crud/Category";
+import Tag from "../../../components/crud/Tag";
 
-const CategoryTag = () => (
-  <Layout>
-    <Admin>
-      <div className="container-fluid">
-        <div className="row">
-          <div className="col-md-12 pt-5 pb-5">
-            <h2>Manage Categories and Tags</h2>
-          </div>
-          <div className="col-md-4">
-            <p>Categories </p>
-            <Category />
-          </div>
-          <div className="col-md-8">
-            <p>Tags</p>
+const CategoryTag = () => {
+  return (
+    <Layout>
+      <Admin>
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-md-12 pt-5 pb-5">
+              <h2>Manage Categories and Tags</h2>
+            </div>
+            <div className="col-md-6">
+              <Category />
+            </div>
+            <div className="col-md-6">
+              <Tag />
+            </div>
           </div>
         </div>
-      </div>
-    </Admin>
-  </Layout>
-);
+      </Admin>
+    </Layout>
+  );
+};
 export default CategoryTag;
