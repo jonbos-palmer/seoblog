9b7a9f83456f0739a7ef95eefd40b4881aafdbe2
jonbos-palmer <jbos@thepalmergroup.com>
2020-07-13 07:31:15 -0500

    import requirement into blog controller

--- backend/controllers/blog.js
+++ backend/controllers/blog.js
@@ -1,3 +1,14 @@
+const Blog = require("../models/Blog");
+const Category = require("../models/Category");
+const Tag = require("../models/Tag");
+
+const { errorHandler } = require("../helpers/dbErrorHandler");
+
+const formidable = require("formidable");
+const slugify = require("slugify");
+const stripHtml = require("string-strip-html");
+const _ = require("lodash");
+
 exports.create = (req, res) => {
   res.json({ time: Date().toString() });
 };
