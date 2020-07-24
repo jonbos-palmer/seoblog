2073c0f72d0a9f0c52a0879701b521e0cec56bd4
jonbos-palmer <jbos@thepalmergroup.com>
2020-07-24 09:07:29 -0500

    show cats and tags on blog page

--- frontend/pages/blogs/index.js
+++ frontend/pages/blogs/index.js
@@ -2,6 +2,7 @@ import Layout from "../../components/Layout";
 import { useState } from "react";
 import { listBlogsWithCategoriesAndTags } from "../../actions/blog";
 import Card from "../../components/blog/Card";
+import Link from "next/link";
 const Blogs = ({ blogs, categories, tags, size }) => {
   const showAllBlogs = () => {
     return blogs.map((blog, i) => {
@@ -13,25 +14,50 @@ const Blogs = ({ blogs, categories, tags, size }) => {
       );
     });
   };
+  const showAllTags = (tags) => {
+    return tags.map((tag, i) => {
+      return (
+        <Link href={`/tags/${tag.slug}`} key={i}>
+          <a className="btn btn-outline-primary mr-1 ml-1 mt-3">{tag.name}</a>
+        </Link>
+      );
+    });
+  };
+  const showAllCategories = (categories) => {
+    return categories.map((cat, i) => {
+      return (
+        <Link href={`/categories/${cat.slug}`} key={i}>
+          <a className="btn btn-primary mr-1 ml-1 mt-3">{cat.name}</a>
+        </Link>
+      );
+    });
+  };
+
   return (
     <Layout>
-      <div className="container-fluid">
-        <header>
-          <div className="col-md-12 pt-3">
-            <h1 className="display-4 font-weight-bold text-center">
-              Programming Blogs and Tutorials
-            </h1>
-          </div>
-          <section>
-            <p>Show categories and tags</p>
-          </section>
-        </header>
-      </div>
-      <div className="container-fluid">
-        <div className="row">
-          <div className="col-md-12">{showAllBlogs()}</div>
+      <main>
+        <div className="container-fluid">
+          <header>
+            <div className="col-md-12 pt-3">
+              <h1 className="display-4 font-weight-bold text-center">
+                Programming Blogs and Tutorials
+              </h1>
+            </div>
+            <section>
+              <div className="pb-5 text-center">
+                {showAllCategories(categories)}
+                <br />
+                {showAllTags(tags)}
+              </div>
+            </section>
+          </header>
         </div>
-      </div>
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-md-12">{showAllBlogs()}</div>
+          </div>
+        </div>
+      </main>
     </Layout>
   );
 };
