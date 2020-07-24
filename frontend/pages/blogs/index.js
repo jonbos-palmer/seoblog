79ba9b4d7640d53354bebab05f41a9a0e19f9720
jonbos-palmer <jbos@thepalmergroup.com>
2020-07-24 09:54:38 -0500

    add metadata to head

--- frontend/pages/blogs/index.js
+++ frontend/pages/blogs/index.js
@@ -1,9 +1,42 @@
+import Head from "next/head";
 import Layout from "../../components/Layout";
 import { useState } from "react";
 import { listBlogsWithCategoriesAndTags } from "../../actions/blog";
 import Card from "../../components/blog/Card";
 import Link from "next/link";
-const Blogs = ({ blogs, categories, tags, size }) => {
+import { withRouter } from "next/router";
+import { API, DOMAIN, APP_NAME } from "../../config";
+const Blogs = ({ blogs, categories, tags, size, router }) => {
+  const head = () => {
+    return (
+      <Head>
+        <title>Programming Blogs | {APP_NAME}</title>
+        <meta
+          name="description"
+          content="programming blogs and tutorials on React node next vue laravel"
+        />
+        <link rel="canonical" href={`${DOMAIN}${router.pathname}`} />
+        <meta
+          property="og:title"
+          content={`Latest Development Tutorials | ${APP_NAME}`}
+        />
+        <meta
+          property="og:description"
+          content={`Latest Development Tutorials | ${APP_NAME}`}
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content={`${DOMAIN}${router.pathname}`} />
+        <meta property="og:site_name" content={`${APP_NAME}`} />
+        <meta property="og:image" content="/static/images/blog/main.jpg" />\
+        <meta
+          property="og:image:secure_url"
+          content="/static/images/blog/main.jpg"
+        />
+        <meta property="og:image_type" content="image/jpg" />
+      </Head>
+    );
+  };
+
   const showAllBlogs = () => {
     return blogs.map((blog, i) => {
       return (
@@ -34,31 +67,34 @@ const Blogs = ({ blogs, categories, tags, size }) => {
   };
 
   return (
-    <Layout>
-      <main>
-        <div className="container-fluid">
-          <header>
-            <div className="col-md-12 pt-3">
-              <h1 className="display-4 font-weight-bold text-center">
-                Programming Blogs and Tutorials
-              </h1>
-            </div>
-            <section>
-              <div className="pb-5 text-center">
-                {showAllCategories(categories)}
-                <br />
-                {showAllTags(tags)}
+    <React.Fragment>
+      {head()}
+      <Layout>
+        <main>
+          <div className="container-fluid">
+            <header>
+              <div className="col-md-12 pt-3">
+                <h1 className="display-4 font-weight-bold text-center">
+                  Programming Blogs and Tutorials
+                </h1>
               </div>
-            </section>
-          </header>
-        </div>
-        <div className="container-fluid">
-          <div className="row">
-            <div className="col-md-12">{showAllBlogs()}</div>
+              <section>
+                <div className="pb-5 text-center">
+                  {showAllCategories(categories)}
+                  <br />
+                  {showAllTags(tags)}
+                </div>
+              </section>
+            </header>
           </div>
-        </div>
-      </main>
-    </Layout>
+          <div className="container-fluid">
+            <div className="row">
+              <div className="col-md-12">{showAllBlogs()}</div>
+            </div>
+          </div>
+        </main>
+      </Layout>
+    </React.Fragment>
   );
 };
 Blogs.getInitialProps = () => {
@@ -75,4 +111,4 @@ Blogs.getInitialProps = () => {
     }
   });
 };
-export default Blogs;
+export default withRouter(Blogs);
